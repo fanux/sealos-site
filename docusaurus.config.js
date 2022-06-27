9bf0138245b1bb3e104fc135e4c31ed8209ba81f
Mercurio <signormercurio@gmail.com>
2022-06-28 00:11:34 +0800

    update repo info in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,100 +1,87 @@
 module.exports = {
-  title: 'sealos',
-  tagline: 'kubernetes-kernel-based cloud os! Let\'s sealos run kubernetes',
-  url: 'https://sealos.io',
-  baseUrl: '/',
-  favicon: 'img/sealos.ico',
-  organizationName: 'labring', // Usually your GitHub org/user name.
-  projectName: 'sealos', // Usually your repo name.
+  title: "sealos",
+  tagline: "kubernetes-kernel-based cloud os! Let's sealos run kubernetes",
+  url: "https://docs.sealos.io",
+  baseUrl: "/",
+  favicon: "img/sealos.ico",
+  organizationName: "labring", // Usually your GitHub org/user name.
+  projectName: "sealos", // Usually your repo name.
   themeConfig: {
     navbar: {
       logo: {
-        alt: 'sealos',
-        src: 'img/sealos-left.png',
+        alt: "sealos",
+        src: "img/sealos-left.png",
       },
       links: [
         {
-          to: 'docs/doc1',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/doc1",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/labring/sealos",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'light',
+      style: "light",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Style Guide',
-              to: 'docs/doc1',
+              label: "Style Guide",
+              to: "docs/doc1",
             },
             {
-              label: 'Second Doc',
-              to: 'docs/doc2',
+              label: "Second Doc",
+              to: "docs/doc2",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: "GitHub",
+              href: "https://github.com/labring/sealos",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} sealos. Built with Docusaurus.`,
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/labring/sealos-site/edit/master/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
