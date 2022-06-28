653f3f40df516c22a7f5efd57c8af1d03848c3ae
zzjin <zzjin@users.noreply.github.com>
2022-06-28 14:58:22 +0000

    fix tabs render code highlights. Closes #13.

--- src/components/Homepage/Examples/index.js
+++ src/components/Homepage/Examples/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import clsx from "clsx";
+import MDXContent from "@theme/MDXContent";
 import ExamplesBlock from "./_examples_block.mdx";
 import styles from "./styles.module.css";
 
@@ -12,7 +13,9 @@ export default function HomepageExamples() {
             <h3>Instant Create Examples</h3>
           </div>
           <div className={clsx("col col--12")}>
-            <ExamplesBlock />
+            <MDXContent>
+              <ExamplesBlock />
+            </MDXContent>
           </div>
         </div>
       </div>
