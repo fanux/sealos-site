4a58dcb9612c8af5b6752f8116f2bc0a3400904b
Mercurio <signormercurio@gmail.com>
2022-07-08 21:46:20 +0800

    fix editURL to edit localized files
    Signed-off-by: Mercurio <signormercurio@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,12 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/fanux/sealos-site/tree/main/",
+          editLocalizedFiles: true,
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/fanux/sealos-site/tree/main/",
+          editLocalizedFiles: true,
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
