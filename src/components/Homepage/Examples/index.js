1d882f1b6a361b6e3512902be35c5ca9867b0ec1
zzjin <zzjin@users.noreply.github.com>
2022-11-04 17:59:21 +0800

    Add application example. (#57)
    Signed-off-by: zzjin <tczzjin@gmail.com>
    
    Signed-off-by: zzjin <tczzjin@gmail.com>

--- src/components/Homepage/Examples/index.js
+++ src/components/Homepage/Examples/index.js
@@ -1,8 +1,9 @@
-import Translate from '@docusaurus/Translate';
+import Translate from "@docusaurus/Translate";
 import MDXContent from "@theme/MDXContent";
 import clsx from "clsx";
 import React from "react";
 import styles from "./styles.module.css";
+import ExamplesAppBlock from "./_examples_app_block.mdx";
 import ExamplesBlock from "./_examples_block.mdx";
 
 export default function HomepageExamples() {
@@ -20,6 +21,20 @@ export default function HomepageExamples() {
           <div className={clsx("col col--12")}>
             <MDXContent>
               <ExamplesBlock />
+            </MDXContent>
+          </div>
+        </div>
+        <div className="row">
+          <div className={clsx("col")}>
+            <h3>
+              <Translate description="homepage instant application examples">
+                Instant Application Examples
+              </Translate>
+            </h3>
+          </div>
+          <div className={clsx("col col--12")}>
+            <MDXContent>
+              <ExamplesAppBlock />
             </MDXContent>
           </div>
         </div>
