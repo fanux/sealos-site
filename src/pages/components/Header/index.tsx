fa6e264d8f61af633d5349d4b0652e3599f25494
zhujingyang <72259332+zjy365@users.noreply.github.com>
2023-07-25 16:17:10 +0800

    feat add i18n (#109)

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -6,16 +6,18 @@ import LogoIcon from '@site/static/icons/sealos.svg'
 import React, { useEffect, useState } from 'react'
 import VideoPlayer from '../VideoPlayer'
 import './index.scss'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import useIsBrowser from '@docusaurus/useIsBrowser'
 
 const navbar = [
   {
     key: 'docs',
-    label: 'Documentation',
+    label: <Translate>Documentation</Translate>,
     to: '/docs/Intro',
   },
   {
     key: 'community',
-    label: 'Community',
+    label: <Translate>Community</Translate>,
     to: 'https://forum.laf.run/',
   },
   {
@@ -25,23 +27,29 @@ const navbar = [
   },
 ]
 
+const i18nObj = {
+  startNow: <Translate>Start Now</Translate>,
+  cloudOS: <Translate>Cloud Operating System</Translate>,
+}
+
 const HomeHeader = ({ isPc }: { isPc: boolean }) => {
   const [stars, setStars] = useState(10000)
+  const isBrowser = useIsBrowser()
 
-  // const i18nMap: { [key: string]: { label: string; link: string } } = {
-  //   en: { label: '中', link: '/zh-Hans/' },
-  //   ['zh-Hans']: { label: 'En', link: '/' },
-  // }
+  const i18nMap: { [key: string]: { label: string; link: string } } = {
+    en: { label: '中', link: '/zh-Hans/' },
+    ['zh-Hans']: { label: 'En', link: '/' },
+  }
 
-  // const {
-  //   i18n: { currentLocale },
-  //   siteConfig: {
-  //     themeConfig: {
-  //       // @ts-ignore nextLine
-  //       navbar: { items: navbarData },
-  //     },
-  //   },
-  // } = useDocusaurusContext()
+  const {
+    i18n: { currentLocale },
+    siteConfig: {
+      themeConfig: {
+        // @ts-ignore nextLine
+        navbar: { items: navbarData },
+      },
+    },
+  } = useDocusaurusContext()
 
   useEffect(() => {
     ;(async () => {
@@ -92,22 +100,40 @@ const HomeHeader = ({ isPc }: { isPc: boolean }) => {
           </div>
         </nav>
         <main>
-          <h1>
-            <span className="txt-title">Kubernetes&nbsp;</span>
-            <span className="txt-aid">as the kernel</span>
-          </h1>
-          <h2>Cloud Operating System</h2>
-          <h3>
-            Abstracting the entire data center as a singular server, where
-            everything is an application. You can use Sealos as seamlessly as
-            operating a&nbsp;
-            <span className="txt-title">personal computer.</span>
-          </h3>
+          {currentLocale === 'en' ? (
+            <h1>
+              <span className="txt-title">Kubernetes&nbsp;</span>
+              <span className="txt-aid">as the kernel</span>
+            </h1>
+          ) : (
+            <h1>
+              <span className="txt-aid">以&nbsp;</span>
+              <span className="txt-title">Kubernetes&nbsp;</span>
+              <span className="txt-aid">为内核</span>
+            </h1>
+          )}
+
+          <h2>{i18nObj.cloudOS}</h2>
+
+          {currentLocale === 'en' ? (
+            <h3>
+              Abstracting the entire data center as a singular server, where
+              everything is an application. You can use Sealos as seamlessly as
+              operating a&nbsp;
+              <span className="txt-title">personal computer.</span>
+            </h3>
+          ) : (
+            <h3>
+              整个数据中心抽象成一台服务器，一切皆应用，像使用
+              <span className="txt-title">个人电脑</span>
+              一样使用 Sealos！
+            </h3>
+          )}
           <a
             className="start-now-button"
             href="https://cloud.sealos.io"
             target="_blank">
-            Start Now
+            {i18nObj.startNow}
             <div className="start-now-button-wrap"></div>
           </a>
           <VideoPlayer
@@ -142,42 +168,59 @@ const HomeHeader = ({ isPc }: { isPc: boolean }) => {
         </div>
 
         <div className="right">
-          {/* {isBrowser ? (
-            <div className="right">
+          <Link className="git-icon" to="https://github.com/labring/sealos">
+            <GithubIcon width={'20px'} height={'20px'} color="#fff" />
+            <span className="git-stars">{(stars / 1000).toFixed(1)}k</span>
+          </Link>
+          {isBrowser && (
+            <div className="i18nIcon">
               <Link
                 to={`${location.origin}${i18nMap[currentLocale]?.link}`}
                 target="_self">
                 {i18nMap[currentLocale]?.label}
               </Link>
             </div>
-          ) : (
-            <div className="right">中</div>
-          )} */}
-          <Link className="git-icon" to="https://github.com/labring/sealos">
-            <GithubIcon width={'20px'} height={'20px'} color="#fff" />
-            <span className="git-stars">{(stars / 1000).toFixed(1)}k</span>
-          </Link>
+          )}
           <a
             className="start-now-button"
             href="https://cloud.sealos.io"
             target="_blank">
-            Start Now
+            {i18nObj.startNow}
             <div className="start-now-button-wrap"></div>
           </a>
         </div>
       </nav>
       <main>
-        <h1>
-          <span className="txt-title">Kubernetes&nbsp;</span>
-          <span className="txt-aid">as the kernel</span>
-        </h1>
-        <h2>Cloud Operating System</h2>
-        <h3>
-          Abstracting the entire data center as a singular server, where
-          everything is an application. You can use Sealos as seamlessly as
-          operating a&nbsp;
-          <span className="txt-title">personal computer.</span>
-        </h3>
+        {currentLocale === 'en' ? (
+          <h1>
+            <span className="txt-title">Kubernetes&nbsp;</span>
+            <span className="txt-aid">as the kernel</span>
+          </h1>
+        ) : (
+          <h1>
+            <span className="txt-aid">以&nbsp;</span>
+            <span className="txt-title">Kubernetes&nbsp;</span>
+            <span className="txt-aid">为内核</span>
+          </h1>
+        )}
+
+        <h2>{i18nObj.cloudOS}</h2>
+
+        {currentLocale === 'en' ? (
+          <h3>
+            Abstracting the entire data center as a singular server, where
+            everything is an application. You can use Sealos as seamlessly as
+            operating a&nbsp;
+            <span className="txt-title">personal computer.</span>
+          </h3>
+        ) : (
+          <h3>
+            整个数据中心抽象成一台服务器，一切皆应用，像使用
+            <span className="txt-title">个人电脑</span>
+            一样使用 Sealos！
+          </h3>
+        )}
+
         <VideoPlayer
           url={
             'https://itceb8-video.oss.laf.run/sealos-website.mp4'
